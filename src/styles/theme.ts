e3a5ce42bbcc7a3787d89201f7dfe6041260c082
Adrian Bradu <adrian.bradu@gmail.com>
2022-12-18 16:36:17 +0200

    updated theme with colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,15 @@
 export const theme = {
   colors: {
     primary: '#7476ED',
-    copy: '#83828E',
+    grey: '#83828E',
+    lightGrey: '#A5A5AA',
+    darkGrey: '#9897A1',
     white: '#ffffff',
     pale: '#F8F4FF',
     violet: '#3A3B7B',
+    darkViolet: '#312E43',
+    copy: '#6F6D7B',
+    error: '#F83E5A',
+    pagopa: '#BF7EE6',
   },
 };
